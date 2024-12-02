5d1d9155bc0b30aa1e3bb588a017c94489b5d4df
Raha Heidari <heidariraha761@gmail.com>
2024-12-02 11:56:26 -0800

    q2

---
+++ js-10-q2.js
@@ -0,0 +1,13 @@
+// Define any car using an object with 4 properties, like: car name, model, color and etc.
+// Then access the car’s name value in the console with the way you’ve learned
+
+//! Answer:
+
+let car = {
+ nameCar: "BMW",
+ modelCar: "X5",
+ colorCar: ["white", "black"],
+ priceCar: 449.999,
+}
+
+console.log(car.nameCar);
