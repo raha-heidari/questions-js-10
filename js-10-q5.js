41aa8d90d1e79cd57001ff61df0a15745753ade7
Raha Heidari <heidariraha761@gmail.com>
2024-12-02 11:56:47 -0800

    q5

---
+++ js-10-q5.js
@@ -0,0 +1,22 @@
+/*
+ ? look at the code below:
+ todo: 1. create an object which has three properties. 
+ todo: 2. use "for in" to show below result: 
+ -Mike is tall, 
+ -Jake is kind, 
+ -Lilly is young 
+ structure: ({key} is {value})
+*/
+
+/**Write your code below  */
+
+const description ={
+    Mike: "tall",
+    Jake: "kind",
+    Lilly: "young",
+}
+
+for(const person in description){
+    console.log(`${person} is ${description[person]}`);
+    
+}
