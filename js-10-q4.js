439530c095e6c632d2732cc3312144206d7ddfbc
Raha Heidari <heidariraha761@gmail.com>
2024-12-02 11:56:40 -0800

    q4

---
+++ js-10-q4.js
@@ -0,0 +1,10 @@
+// What is the result of the code below?
+const hello = "what's up";
+const obj = {
+  func() {
+    return `${hello} aps?!`;
+  },
+};
+console.log(obj.func()); // answer is?:  what's up aps?!
+
+// this object has a property called funk, which is a function. First, the func function is called and the function creates the formatted string and returns it.
